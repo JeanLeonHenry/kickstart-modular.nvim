55ddd0f8f8ea634d5ee30411ed8421bdc4cb2c10
Jean-Léon Henry <jeanleon.henry@protonmail.com>
2024-09-22 12:49:48 +0200

    feat: ollama support

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,7 +6,24 @@ return {
   'tpope/vim-fugitive',
   {
     'nvim-lualine/lualine.nvim',
-    opts = {},
+    opts = {
+      function(_, opts)
+        table.insert(opts.sections.lualine_x, {
+          function()
+            local status = require('ollama').status()
+
+            if status == 'IDLE' then
+              return '󱙺' -- nf-md-robot-outline
+            elseif status == 'WORKING' then
+              return '󰚩' -- nf-md-robot
+            end
+          end,
+          cond = function()
+            return package.loaded['ollama'] and require('ollama').status() ~= nil
+          end,
+        })
+      end,
+    },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
 }
