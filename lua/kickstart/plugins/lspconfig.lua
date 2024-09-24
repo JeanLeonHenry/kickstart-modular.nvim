466ca89b8a49cc8206310705634dcc0da0f52066
Jean-Léon Henry <jeanleon.henry@protonmail.com>
2024-09-24 20:33:21 +0200

    feat: added bash lsp

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -178,6 +178,7 @@ return {
             })
           end,
         },
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
